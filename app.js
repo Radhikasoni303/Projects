220263cdad8764c5075903588169aadda3c02951
Radhika Soni <soniradhika303@gmail.com>
2023-08-23 11:53:55 +0530

    Add new files

---
+++ app.js
@@ -0,0 +1,28 @@
+const form = document.querySelector('form')
+form.addEventListener('submit',function(e){
+    e.preventDefault()
+
+const height = parseInt(document.querySelector('#height').value)
+const weight = parseInt(document.querySelector('#height').value)
+const results =document.querySelector('#results')
+
+
+if(height === '' || height < 0 || isNaN(height)){
+  results.innerHTML = `Please give a valid height ${height}`;
+}else if(weight === '' || weight < 0 || isNaN(weight)){
+    results.innerHTML = `Please give a valid weight ${weight}`;
+}else {
+    const bmi = (weight / ((height* height)/10000)).toFixed(2);
+    results.innerHTML = `<span>${bmi}</span>`;
+  }
+
+  const guide =document.querySelector('#guide')
+  if(results >= 18.6){
+    guide.innerHTML = "Under Weight";
+  }else if(results <= 24.9){
+    guide.innerHTML = "Normal Weight";
+  }else {
+    guide.innerHTML = "Over Weight";
+  }
+
+});
